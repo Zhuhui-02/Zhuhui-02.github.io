492921128c01844ecaf5bf69787df53e39420aad
Zhuhui-02 <LiuZH0.0@outlook.com>
2026-07-19 15:43:04 +0800

    Add animal specimen review and submissions

---
+++ animals/data.js
@@ -0,0 +1,948 @@
+window.ANIMAL_CARDS = [
+  {
+    "id": "animal-001",
+    "number": 1,
+    "name": "日本菊花螺",
+    "images": [
+      "assets/001-日本菊花螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-002",
+    "number": 2,
+    "name": "史氏背尖贝",
+    "images": [
+      "assets/002-史氏背尖贝-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-003",
+    "number": 3,
+    "name": "桂山后丛柳珊瑚",
+    "images": [
+      "assets/003-桂山后丛柳珊瑚-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-004",
+    "number": 4,
+    "name": "日本鼓虾",
+    "images": [
+      "assets/004-日本鼓虾-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-005",
+    "number": 5,
+    "name": "单齿螺",
+    "images": [
+      "assets/005-单齿螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-006",
+    "number": 6,
+    "name": "日本大眼蟹",
+    "images": [
+      "assets/006-日本大眼蟹-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-007",
+    "number": 7,
+    "name": "条纹隔贻贝",
+    "images": [
+      "assets/007-条纹隔贻贝-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-008",
+    "number": 8,
+    "name": "下齿细螯寄居蟹",
+    "images": [
+      "assets/008-下齿细螯寄居蟹-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-009",
+    "number": 9,
+    "name": "纵肋织纹螺",
+    "images": [
+      "assets/009-纵肋织纹螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-010",
+    "number": 10,
+    "name": "日本笠藤壶",
+    "images": [
+      "assets/010-日本笠藤壶-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-011",
+    "number": 11,
+    "name": "海蟑螂",
+    "images": [
+      "assets/011-海蟑螂-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-012",
+    "number": 12,
+    "name": "短滨螺",
+    "images": [
+      "assets/012-短滨螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-013",
+    "number": 13,
+    "name": "橄榄拳蟹",
+    "images": [
+      "assets/013-橄榄拳蟹-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-014",
+    "number": 14,
+    "name": "疣荔枝螺",
+    "images": [
+      "assets/014-疣荔枝螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-015",
+    "number": 15,
+    "name": "毛贻贝",
+    "images": [
+      "assets/015-毛贻贝-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-016",
+    "number": 16,
+    "name": "微黄镰玉螺",
+    "images": [
+      "assets/016-微黄镰玉螺-1.jpg",
+      "assets/016-微黄镰玉螺-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-017",
+    "number": 17,
+    "name": "银口凹螺",
+    "images": [
+      "assets/017-银口凹螺-1.jpg",
+      "assets/017-银口凹螺-2.jpg",
+      "assets/017-银口凹螺-3.jpg"
+    ]
+  },
+  {
+    "id": "animal-018",
+    "number": 18,
+    "name": "贻贝",
+    "images": [
+      "assets/018-贻贝-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-019",
+    "number": 19,
+    "name": "爪哇拟塔螺",
+    "images": [
+      "assets/019-爪哇拟塔螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-020",
+    "number": 20,
+    "name": "近江牡蛎",
+    "images": [
+      "assets/020-近江牡蛎-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-021",
+    "number": 21,
+    "name": "绒螯近方蟹",
+    "images": [
+      "assets/021-绒螯近方蟹-1.jpg",
+      "assets/021-绒螯近方蟹-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-022",
+    "number": 22,
+    "name": "龟足",
+    "images": [
+      "assets/022-龟足-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-023",
+    "number": 23,
+    "name": "红条毛肤石鳖",
+    "images": [
+      "assets/023-红条毛肤石鳖-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-024",
+    "number": 24,
+    "name": "尖锥拟蟹守螺",
+    "images": [
+      "assets/024-尖锥拟蟹守螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-025",
+    "number": 25,
+    "name": "等指海葵",
+    "images": [
+      "assets/025-等指海葵-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-026",
+    "number": 26,
+    "name": "异足索沙蚕",
+    "images": [
+      "assets/026-异足索沙蚕-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-027",
+    "number": 27,
+    "name": "斑尾复鰕虎鱼",
+    "images": [
+      "assets/027-斑尾复鰕虎鱼-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-028",
+    "number": 28,
+    "name": "红狼牙鰕虎鱼",
+    "images": [
+      "assets/028-红狼牙鰕虎鱼-1.jpg",
+      "assets/028-红狼牙鰕虎鱼-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-029",
+    "number": 29,
+    "name": "缘齿牡蛎",
+    "images": [
+      "assets/029-缘齿牡蛎-1.jpg",
+      "assets/029-缘齿牡蛎-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-030",
+    "number": 30,
+    "name": "珠带拟蟹守螺",
+    "images": [
+      "assets/030-珠带拟蟹守螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-031",
+    "number": 31,
+    "name": "甲虫螺",
+    "images": [
+      "assets/031-甲虫螺-1.jpg",
+      "assets/031-甲虫螺-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-032",
+    "number": 32,
+    "name": "豆形拳蟹",
+    "images": [
+      "assets/032-豆形拳蟹-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-033",
+    "number": 33,
+    "name": "半褶织纹螺",
+    "images": [
+      "assets/033-半褶织纹螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-034",
+    "number": 34,
+    "name": "紫藤斧蛤",
+    "images": [
+      "assets/034-紫藤斧蛤-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-035",
+    "number": 35,
+    "name": "太平洋侧花海葵",
+    "images": [
+      "assets/035-太平洋侧花海葵-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-036",
+    "number": 36,
+    "name": "白背藤壶",
+    "images": [
+      "assets/036-白背藤壶-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-037",
+    "number": 37,
+    "name": "胶薮枝螅",
+    "images": [
+      "assets/037-胶薮枝螅-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-038",
+    "number": 38,
+    "name": "平背蜞",
+    "images": [
+      "assets/038-平背蜞-1.jpg",
+      "assets/038-平背蜞-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-039",
+    "number": 39,
+    "name": "红带织纹螺",
+    "images": [
+      "assets/039-红带织纹螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-040",
+    "number": 40,
+    "name": "粗腿厚纹蟹",
+    "images": [
+      "assets/040-粗腿厚纹蟹-1.jpg",
+      "assets/040-粗腿厚纹蟹-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-041",
+    "number": 41,
+    "name": "粒花冠小月螺",
+    "images": [
+      "assets/041-粒花冠小月螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-042",
+    "number": 42,
+    "name": "青蚶",
+    "images": [
+      "assets/042-青蚶-1.jpg",
+      "assets/042-青蚶-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-043",
+    "number": 43,
+    "name": "黑凹螺",
+    "images": [
+      "assets/043-黑凹螺-1.jpg",
+      "assets/043-黑凹螺-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-044",
+    "number": 44,
+    "name": "矮拟帽螺",
+    "images": [
+      "assets/044-矮拟帽螺-1.jpg",
+      "assets/044-矮拟帽螺-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-045",
+    "number": 45,
+    "name": "藻钩虾",
+    "images": [
+      "assets/045-藻钩虾-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-046",
+    "number": 46,
+    "name": "婆罗囊螺",
+    "images": [
+      "assets/046-婆罗囊螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-047",
+    "number": 47,
+    "name": "齿纹蜒螺",
+    "images": [
+      "assets/047-齿纹蜒螺-1.jpg",
+      "assets/047-齿纹蜒螺-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-048",
+    "number": 48,
+    "name": "锈凹螺",
+    "images": [
+      "assets/048-锈凹螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-049",
+    "number": 49,
+    "name": "长螯活额寄居蟹",
+    "images": [
+      "assets/049-长螯活额寄居蟹-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-050",
+    "number": 50,
+    "name": "斧光蛤蜊",
+    "images": [
+      "assets/050-斧光蛤蜊-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-051",
+    "number": 51,
+    "name": "秀丽织纹螺",
+    "images": [
+      "assets/051-秀丽织纹螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-052",
+    "number": 52,
+    "name": "宽身大眼蟹",
+    "images": [
+      "assets/052-宽身大眼蟹-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-053",
+    "number": 53,
+    "name": "凸镜蛤",
+    "images": [
+      "assets/053-凸镜蛤-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-054",
+    "number": 54,
+    "name": "瘤荔枝螺",
+    "images": [
+      "assets/054-瘤荔枝螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-055",
+    "number": 55,
+    "name": "圆筒原盒螺",
+    "images": [
+      "assets/055-圆筒原盒螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-056",
+    "number": 56,
+    "name": "小相手蟹",
+    "images": [
+      "assets/056-小相手蟹-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-057",
+    "number": 57,
+    "name": "鳞笠藤壶",
+    "images": [
+      "assets/057-鳞笠藤壶-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-058",
+    "number": 58,
+    "name": "嫁䗩",
+    "images": [
+      "assets/058-嫁䗩-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-059",
+    "number": 59,
+    "name": "海地瓜",
+    "images": [
+      "assets/059-海地瓜-1.jpg",
+      "assets/059-海地瓜-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-060",
+    "number": 60,
+    "name": "彩虹明樱蛤",
+    "images": [
+      "assets/060-彩虹明樱蛤-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-061",
+    "number": 61,
+    "name": "长腕寄居蟹",
+    "images": [
+      "assets/061-长腕寄居蟹-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-062",
+    "number": 62,
+    "name": "方斑东风螺",
+    "images": [
+      "assets/062-方斑东风螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-063",
+    "number": 63,
+    "name": "花斑锉石鳖",
+    "images": [
+      "assets/063-花斑锉石鳖-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-064",
+    "number": 64,
+    "name": "弧边招潮蟹",
+    "images": [
+      "assets/064-弧边招潮蟹-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-065",
+    "number": 65,
+    "name": "脉红螺",
+    "images": [
+      "assets/065-脉红螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-066",
+    "number": 66,
+    "name": "西格织纹螺",
+    "images": [
+      "assets/066-西格织纹螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-067",
+    "number": 67,
+    "name": "红线黎明蟹",
+    "images": [
+      "assets/067-红线黎明蟹-1.jpg",
+      "assets/067-红线黎明蟹-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-068",
+    "number": 68,
+    "name": "文蛤",
+    "images": [
+      "assets/068-文蛤-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-069",
+    "number": 69,
+    "name": "褐管蛾螺",
+    "images": [
+      "assets/069-褐管蛾螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-070",
+    "number": 70,
+    "name": "地中海贻贝",
+    "images": [
+      "assets/070-地中海贻贝-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-071",
+    "number": 71,
+    "name": "四齿大额蟹",
+    "images": [
+      "assets/071-四齿大额蟹-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-072",
+    "number": 72,
+    "name": "鲜明鼓虾",
+    "images": [
+      "assets/072-鲜明鼓虾-1.jpg",
+      "assets/072-鲜明鼓虾-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-073",
+    "number": 73,
+    "name": "长足长方蟹",
+    "images": [
+      "assets/073-长足长方蟹-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-074",
+    "number": 74,
+    "name": "黑荞麦蛤",
+    "images": [
+      "assets/074-黑荞麦蛤-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-075",
+    "number": 75,
+    "name": "长指寄居蟹",
+    "images": [
+      "assets/075-长指寄居蟹-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-076",
+    "number": 76,
+    "name": "棘刺牡蛎",
+    "images": [
+      "assets/076-棘刺牡蛎-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-077",
+    "number": 77,
+    "name": "粒结节滨螺",
+    "images": [
+      "assets/077-粒结节滨螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-078",
+    "number": 78,
+    "name": "光辉圆扇蟹",
+    "images": [
+      "assets/078-光辉圆扇蟹-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-079",
+    "number": 79,
+    "name": "黄侧花海葵",
+    "images": [
+      "assets/079-黄侧花海葵-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-080",
+    "number": 80,
+    "name": "长纺锤螺",
+    "images": [
+      "assets/080-长纺锤螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-081",
+    "number": 81,
+    "name": "肉球近方蟹",
+    "images": [
+      "assets/081-肉球近方蟹-1.jpg",
+      "assets/081-肉球近方蟹-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-082",
+    "number": 82,
+    "name": "隆背拳蟹",
+    "images": [
+      "assets/082-隆背拳蟹-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-083",
+    "number": 83,
+    "name": "海月",
+    "images": [
+      "assets/083-海月-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-084",
+    "number": 84,
+    "name": "黄口荔枝螺",
+    "images": [
+      "assets/084-黄口荔枝螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-085",
+    "number": 85,
+    "name": "毛蚶",
+    "images": [
+      "assets/085-毛蚶-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-086",
+    "number": 86,
+    "name": "扁玉螺",
+    "images": [
+      "assets/086-扁玉螺-1.jpg",
+      "assets/086-扁玉螺-2.jpg",
+      "assets/086-扁玉螺-3.jpg"
+    ]
+  },
+  {
+    "id": "animal-087",
+    "number": 87,
+    "name": "格䗩",
+    "images": [
+      "assets/087-格䗩-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-088",
+    "number": 88,
+    "name": "托氏䗉螺",
+    "images": [
+      "assets/088-托氏䗉螺-1.jpg",
+      "assets/088-托氏䗉螺-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-089",
+    "number": 89,
+    "name": "结蚶",
+    "images": [
+      "assets/089-结蚶-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-090",
+    "number": 90,
+    "name": "中华水虱",
+    "images": [
+      "assets/090-中华水虱-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-091",
+    "number": 91,
+    "name": "泥螺",
+    "images": [
+      "assets/091-泥螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-092",
+    "number": 92,
+    "name": "长毛对虾",
+    "images": [
+      "assets/092-长毛对虾-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-093",
+    "number": 93,
+    "name": "斑点相手蟹",
+    "images": [
+      "assets/093-斑点相手蟹-1.jpg",
+      "assets/093-斑点相手蟹-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-094",
+    "number": 94,
+    "name": "帽贝",
+    "images": [
+      "assets/094-帽贝-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-095",
+    "number": 95,
+    "name": "鲻鱼",
+    "images": [
+      "assets/095-鲻鱼-1.jpg",
+      "assets/095-鲻鱼-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-096",
+    "number": 96,
+    "name": "天津厚蟹",
+    "images": [
+      "assets/096-天津厚蟹-1.jpg",
+      "assets/096-天津厚蟹-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-097",
+    "number": 97,
+    "name": "伍氏厚蟹",
+    "images": [
+      "assets/097-伍氏厚蟹-1.jpg",
+      "assets/097-伍氏厚蟹-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-098",
+    "number": 98,
+    "name": "棱鱼",
+    "images": [
+      "assets/098-棱鱼-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-099",
+    "number": 99,
+    "name": "绯拟沼螺",
+    "images": [
+      "assets/099-绯拟沼螺-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-100",
+    "number": 100,
+    "name": "蓝绿细螯寄居蟹",
+    "images": [
+      "assets/100-蓝绿细螯寄居蟹-1.jpg",
+      "assets/100-蓝绿细螯寄居蟹-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-101",
+    "number": 101,
+    "name": "龙头鱼",
+    "images": [
+      "assets/101-龙头鱼-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-102",
+    "number": 102,
+    "name": "鳐",
+    "images": [
+      "assets/102-鳐-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-103",
+    "number": 103,
+    "name": "背眼尖齿鰕虎鱼",
+    "images": [
+      "assets/103-背眼尖齿鰕虎鱼-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-104",
+    "number": 104,
+    "name": "日本方头鱼",
+    "images": [
+      "assets/104-日本方头鱼-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-105",
+    "number": 105,
+    "name": "刀鲚",
+    "images": [
+      "assets/105-刀鲚-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-106",
+    "number": 106,
+    "name": "带鱼",
+    "images": [
+      "assets/106-带鱼-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-107",
+    "number": 107,
+    "name": "银鲳",
+    "images": [
+      "assets/107-银鲳-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-108",
+    "number": 108,
+    "name": "马面鲀",
+    "images": [
+      "assets/108-马面鲀-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-109",
+    "number": 109,
+    "name": "尖头斜齿鲨",
+    "images": [
+      "assets/109-尖头斜齿鲨-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-110",
+    "number": 110,
+    "name": "路氏双髻鲨",
+    "images": [
+      "assets/110-路氏双髻鲨-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-111",
+    "number": 111,
+    "name": "鲈鱼",
+    "images": [
+      "assets/111-鲈鱼-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-112",
+    "number": 112,
+    "name": "黄鲷",
+    "images": [
+      "assets/112-黄鲷-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-113",
+    "number": 113,
+    "name": "台湾东风螺",
+    "images": [
+      "assets/113-台湾东风螺-1.jpg",
+      "assets/113-台湾东风螺-2.jpg"
+    ]
+  },
+  {
+    "id": "animal-114",
+    "number": 114,
+    "name": "鳗鰕虎鱼",
+    "images": [
+      "assets/114-鳗鰕虎鱼-1.jpg"
+    ]
+  },
+  {
+    "id": "animal-115",
+    "number": 115,
+    "name": "褐菖鮋",
+    "images": [
+      "assets/115-褐菖鮋-1.jpg"
+    ]
+  }
+];
